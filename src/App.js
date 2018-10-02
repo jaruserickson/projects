4bd1f9d44aa227306a8ae20a96ad32b206e8c2b1
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2018-10-02 19:49:27 -0400

    Update to read off s3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
     Header,
     Project
 } from './components'
-import projects from './projects.json'
 import FaAngleDown from 'react-icons/lib/fa/angle-down'
 import {
     Element,
@@ -12,41 +11,54 @@ import {
 } from 'react-scroll'
 
 class App extends Component {
+    state = {}
+    JSON_URL = 'https://files.jaruserickson.com/jaruserickson.json'
+
+    loadJson = async () => {
+        const apiResult = await fetch(this.JSON_URL)
+        const siteData = await apiResult.json()
+        this.setState({projects: siteData})
+    }
+
+    componentDidMount() {
+        this.loadJson()
+    }
+
     render() {
-        return (
-            <div className="app">
-                <div className="header-container">
-                    <Header
-                        name={projects.header.name}
-                        subtitle={projects.header.subtitle}
-                        social={projects.header.social}
-                    />
-                    <Link 
-                        to="projects" 
-                        className="down"
-                        smooth={true} 
-                        duration={500}
-                    >
-                        <FaAngleDown size={38} color={'rgba(255,255,255,0.8)'}/>
-                    </Link>
+        return this.state.projects ? (
+                <div className="app">
+                    <div className="header-container">
+                        <Header
+                            name={this.state.projects.header.name}
+                            subtitle={this.state.projects.header.subtitle}
+                            social={this.state.projects.header.social}
+                        />
+                        <Link 
+                            to="projects" 
+                            className="down"
+                            smooth={true} 
+                            duration={500}
+                        >
+                            <FaAngleDown size={38} color={'rgba(255,255,255,0.8)'}/>
+                        </Link>
+                    </div>
+                    <Element name="projects" /> 
+                    <p className="projects-title">projects</p>
+                    <div className="projects">
+                        {
+                            this.state.projects.projects.map((project) => (
+                                <Project {...project} />
+                            ))
+                        }
+                    </div>
+                    <div className="bg" style={{
+                        background: this.state.projects.background + ' no-repeat',
+                        backgroundPosition: 'top',
+                        backgroundSize: '100% 100%',
+                        backgroundSize: 'cover'
+                    }}/>
                 </div>
-                <Element name="projects" /> 
-                <p className="projects-title">projects</p>
-                <div className="projects">
-                    {
-                        projects.projects.map((project) => (
-                            <Project {...project} />
-                        ))
-                    }
-                </div>
-                <div className="bg" style={{
-                    background: projects.background + ' no-repeat',
-                    backgroundPosition: 'top',
-                    backgroundSize: '100% 100%',
-                    backgroundSize: 'cover'
-                }}/>
-            </div>
-        )
+            ) : null
     }
 }
 
